0b4c155b5f3b83f971e3f5a9e6e0da08b5d9b2ea
Joao Costa <joaopedroguedesdacosta@gmail.com>
2022-12-19 13:02:21 +0000

    adicionar botao add as propostas

--- resources/views/livewire/crud.blade.php
+++ resources/views/livewire/crud.blade.php
@@ -1,9 +1,17 @@
 <div>   
-    <center> 
+    
+    <x-jet-button wire:click="createShowModal()">
+                    Criar
+                </x-jet-button>
     <div class="flex flex-col">
+    
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+
+            
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                
                     <table class="min-w-full divide-y divide-gray-200">
                         <thead>
                             <tr>
@@ -25,7 +33,7 @@
                                         <td class="px-6 py-2 flex justify-end">
                                             <x-jet-button wire:click="updateShowModal({{ $item->id }})">
                                                 Editar
-                                            </x-jet-button>
+                                            </x-jet-button> 
                                             <x-jet-danger-button class="ml-2" wire:click="deleteShowModal({{ $item->id }})">
                                                 Eliminar
                                             </x-jet-button>
@@ -44,7 +52,7 @@
             </div>
         </div>
     </div>
-    </center>
+   
     <div class="mt-5">
     {{ $data->links() }}
     </div>
